6c2228d60f7b3defd7cf6351c81dff12d86bf984
Guillermociria <guijai34@gmail.com>
2026-02-24 12:47:00 +0000

    fix: Fixed CI not working
    Added dependencies for the CI to wrok, also added Dummy test

---
+++ mobile/__tests__/dummy.test.ts
@@ -0,0 +1,11 @@
+describe('Dummy Test TS', () => {
+  it('suma correctamente 2 + 2', () => {
+    const result = 2 + 2;
+    expect(result).toBe(4);
+  });
+
+  it('siempre pasa', () => {
+    const truth = true;
+    expect(truth).toBe(true);
+  });
+});
