f879eb85aa6a9c140af426a1d06512bcdd026327
Rosa María Espinosa Martínez <rosespmar@alum.us.es>
2026-02-28 18:06:46 +0000

    feat: add MyArticles screen to AppNavigator

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -7,6 +7,7 @@ import { useAuth } from '../context/AuthContext';
 import LoginScreen       from '../screens/auth/LoginScreen';
 import RegisterScreen    from '../screens/auth/RegisterScreen';
 import HomeScreen from '../screens/home/HomeScreen';
+import MyArticlesScreen from '../screens/profile/MyArticlesScreen';
 import { RootStackParamList } from '../types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -31,6 +32,7 @@ const AppNavigator: React.FC = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="MyArticles" component={MyArticlesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
