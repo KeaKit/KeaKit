827b6b73a0601c290c07a2061d1b0e3956773a0a
Ismael Carrasco Mkhazni <mkhazni2004@gmail.com>
2026-03-02 22:16:52 +0000

    fix: KitService.java

--- backend/src/main/java/com/example/demo/service/KitService.java
+++ backend/src/main/java/com/example/demo/service/KitService.java
@@ -24,6 +24,7 @@ import com.example.demo.repository.ItemRepository;
 import com.example.demo.repository.KitRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.repository.WalletRepository;
+import java.util.ArrayList;
 
 @Service
 public class KitService {
