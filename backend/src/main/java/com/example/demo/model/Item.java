e8047cb154b4b11f5fbbcbb6fa3baf9a49062438
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-02 21:39:29 +0000

    fix: fix tests that failed due to KitItem seeder

--- backend/src/main/java/com/example/demo/model/Item.java
+++ backend/src/main/java/com/example/demo/model/Item.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.*;
 
@@ -36,6 +38,9 @@ public abstract class Item {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     protected User owner;
+
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<KitItem> kitItems = new ArrayList<>();
 
     public Item() {}
 
