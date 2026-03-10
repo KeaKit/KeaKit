2aa74cd91b8b051dc7f5eac26dfb1ab6a156b4e4
AdrianChabrera <adrianchabrera@gmail.com>
2026-03-10 14:21:24 +0000

    feat: add security to user backend endpoints

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .requestMatchers("/h2-console/**").permitAll()
                 //Para asegurar que las url que comiencen por api/admin/ solo serán accesibles por usuarios con rol ADMIN
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/users/**").authenticated()
                 .anyRequest().permitAll()
             )
             .sessionManagement(session -> session
