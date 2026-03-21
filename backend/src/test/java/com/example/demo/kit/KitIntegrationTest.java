d6320f71359d914a787a74f53b386e571da9140d
Enrique <enriquenicolaebarac55@gmail.com>
2026-03-21 17:58:30 +0000

    fix: FIx errors in tests of backend

--- backend/src/test/java/com/example/demo/kit/KitIntegrationTest.java
+++ backend/src/test/java/com/example/demo/kit/KitIntegrationTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
 @ActiveProfiles("test")
+@TestPropertySource(properties = "jwt.secret=TestSecretKeyForJWTTesting123456789012345678901234567890")
 class KitIntegrationTest {
 
     @Autowired
