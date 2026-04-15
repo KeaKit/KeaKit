5685a84d2f7be97ec830db5637f40d283f1854c0
AdrianChabrera <adrianchabrera@gmail.com>
2026-04-15 12:48:57 +0000

    fix: add import deleted in previous merge

--- backend/src/test/java/com/example/demo/kit/KitServiceTest.java
+++ backend/src/test/java/com/example/demo/kit/KitServiceTest.java
@@ -9,6 +9,7 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
